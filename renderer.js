f95cc6db0e8aebeae078012c12e7eaac2b833197
JeffHykin <jeff.hykin@gmail.com>
2017-11-27 23:48:23 -0600

    Touched up size of command

--- renderer.js
+++ renderer.js
@@ -1639,8 +1639,9 @@ sizeof_Command               = new RebyCommand({prefix:"size of ",
                         {
                             a_location = message_.replace(/^(what is the|what'?s the|)size ?of +/,"")
                             file_size = await BashRun('du -h '+Escape(a_location))
-                            file_size = file_size.replace(/(\w+).+/,"$1")
-                            reby.says("That is"+file_size+" large")
+                            file_size = file_size.replace(/\s*(\d+(?:\.\d+|)\w\w?)\s*.+\n/,"$1")
+                            // FIXME, replace K with kilobytes, and Gb with giga bytes etc 
+                            reby.says("That is "+file_size+" large")
                             reby.suggestions = [ "change permissions for "+a_location, "show permissions for "+a_location , "show files","show hidden files", "open [file or folder]"]
                         }
                 }
